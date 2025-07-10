503b49bdb82054ee562b82874a19b81ecf907b9d
Rachel <isa315vc@gmail.com>
2025-07-10 14:05:21 -0400

    feat: Se agrego los comandos para ejecutar el superuser y datos por defecto

--- core/views.py
+++ core/views.py
@@ -1,9 +1,25 @@
 
 from django.core import management
 from django.http import JsonResponse
+from django.contrib.auth import get_user_model
 
+def deploy_all(request):
+    try:
+        management.call_command('migrate')
+        management.call_command('collectstatic', interactive=False)
+        management.call_command('seed_all')
 
-def deploy_migrate(request):
-    management.call_command('migrate')
-    management.call_command('collectstatic', interactive=False)
-    return JsonResponse({'status': 'OK'})
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@admin.com', 'Admin*1234')
+            superuser_status = "Superusuario creado: admin"
+        else:
+            superuser_status = "Superusuario ya existe."
+
+        return JsonResponse({
+            'status': '✅ Todo ejecutado correctamente',
+            'superuser': superuser_status
+        })
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
